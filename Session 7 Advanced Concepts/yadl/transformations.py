f6ff76ee727489d57043c6b730a9852f422a3507
bhuvnk <17757946+bhuvnk@users.noreply.github.com>
2021-06-21 13:39:56 +0530

    Update transformations.py

--- Session 7 Advanced Concepts/yadl/transformations.py
+++ Session 7 Advanced Concepts/yadl/transformations.py
@@ -4,26 +4,19 @@ from albumentations.pytorch import ToTensorV2
 
 def get_train_test_transforms():
     train_transforms = A.Compose(
-      [
-        A.HorizontalFlip(),
-        A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=15, p=0.5),
-        A.CoarseDropout(max_holes = 1,
-                       max_height= 16,
-                       max_width= 16,
-                       min_holes = 1,
-                       min_height= 16,
-                       min_width= 16),
-          
-        A.ToGray(0.1),
-          
-        A.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.247, 0.243, 0.261)),
-        ToTensorV2(),
-      ])
+        [
+            A.Normalize((0.49139968, 0.48215841, 0.44653091), (0.24703223, 0.24348513, 0.26158784)),
+            A.HorizontalFlip(),
+            A.ShiftScaleRotate(),
+            A.CoarseDropout(1, 16, 16, 1, 16, 16,fill_value=0.473363, mask_fill_value=None),
+            A.ToGray()
+#             ToTensorV2(),
+        ])
 
     val_transforms = A.Compose(
         [
-          A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
-          ToTensorV2(),
+            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+#             ToTensorV2(),
         ])
 
     return train_transforms, val_transforms
