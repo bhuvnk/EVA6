6f11b882b5c03b937e211a300075d623ec5352ca
bhuvnk <17757946+bhuvnk@users.noreply.github.com>
2021-06-21 15:53:30 +0530

    Update model.py

--- Session 7 Advanced Concepts/yadl/model.py
+++ Session 7 Advanced Concepts/yadl/model.py
@@ -2,81 +2,95 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
-drop_value = 0.069
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
 
-        # dims : [28,28,1];
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 8, 3, padding = 1, bias=False), # 28
+        # Input Block
+        self.convblock1 = nn.Sequential(
+            nn.Conv2d(3, 32, 3, padding=1, bias=False),
             nn.ReLU(),
-            nn.BatchNorm2d(8),
-            nn.Dropout2d(drop_value),
-            # RF - 3x3
-
-            nn.Conv2d(8, 16, 3, padding = 1, bias=False), # 28
-            nn.ReLU(),
-            nn.BatchNorm2d(16),
-            nn.Dropout2d(drop_value),
-            # RF - 5x5
-        )
-
-        # dims : [28x28x32]
-        # Bottle neck - Transition layer
-
-        self.trans1 = nn.Sequential(
-            nn.Conv2d(16, 8, 1, bias=False), # 28
-            nn.ReLU(),
-            # RF - 7x7
-
-            nn.MaxPool2d(2, 2), # 14
-            # RF - 14x14
-        )
-
-        # dims : [14x14x8]
+            nn.BatchNorm2d(32),
         
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(8, 12, 3, bias=False), # 12
+            # Depthwise Saperable
+            nn.Conv2d(32, 32, 3, padding=1, groups=32, bias=False),
+            nn.Conv2d(32, 64, 1, padding=0, bias=False),
             nn.ReLU(),
-            nn.BatchNorm2d(12),
-            nn.Dropout2d(drop_value),
-            # RF - 16x16
+            nn.BatchNorm2d(64),
+            
+            # nn.Conv2d(64, 64, 3, dilation=8, bias=False),
+            # nn.ReLU(),
+            # nn.BatchNorm2d(64),
+        ) # output_size = 32, receptive field: 7
 
-            nn.Conv2d(12, 12, 3, bias=False), # 10
+        # Transition Block 1
+        self.transblock1 = nn.Sequential(
+            nn.Conv2d(64, 32, 1, padding=0, bias=False),
+        ) # output_size = 32, receptive field: 7
+
+
+        # Convolution Block 2
+        self.convblock2 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, padding=1, bias=False),
             nn.ReLU(),
-            nn.BatchNorm2d(12),
-            nn.Dropout2d(drop_value),
-            # RF - 18x18
-
-            nn.Conv2d(12, 16, 3, bias=False), # 8
+            nn.BatchNorm2d(32),
+            
+            # Depthwise saperable
+            nn.Conv2d(32, 32, 3, padding=1, groups=32, bias=False),
+            nn.Conv2d(32, 64, 1, padding=0, bias=False),
             nn.ReLU(),
-            nn.BatchNorm2d(16),
-            nn.Dropout2d(drop_value),
-            # RF - 20x20
+            nn.BatchNorm2d(64),
 
-            nn.Conv2d(16, 16, 3, bias=False), # 4
+            nn.Conv2d(64, 64, 3, dilation=4, bias=False),
             nn.ReLU(),
-            nn.BatchNorm2d(16),
-            nn.Dropout2d(drop_value),
-            # RF - 22x22
-        )
+            nn.BatchNorm2d(64),
+        ) # output_size = 16, receptive field: 16
 
-        # dims : 4x4x32
+        # Transition Block 2
+        self.transblock2 = nn.Sequential(
+            nn.Conv2d(64, 32, 1, padding=0, bias=False),
+        ) # output_size = 16, receptive field: 16
 
-        # GAP Layer
+        #  Convolution Block 3
+        # Depthwise Separable Convolution
+        self.convblock3 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, padding=1, groups=32, bias=False),
+            nn.Conv2d(32, 64, 1, padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+
+            nn.Conv2d(64, 64, 3, padding=1, groups=64, bias=False),
+            nn.Conv2d(64, 128, 1, padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+        ) # output_size = 4, receptive field: 54
+
+
+         # Output Block 
         self.out = nn.Sequential(
-            nn.AdaptiveAvgPool2d((1,1)),       # 
-            nn.Conv2d(16, 10, 1, bias=False), # 6
-            # RF - 20x20
-        )
+            nn.AdaptiveAvgPool2d((1,1)),       #
+            nn.Conv2d(128, 10, 1, padding=0, bias=False) 
+        ) # output_size = 1, receptive field: 
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.trans1(x)
-        x = self.conv2(x)
-        x = self.out(x)
+        
+        # Input Block: Convolution Block 1
+        x = self.convblock1(x)
+        
+        # Transition Block 1
+        x = self.transblock1(x)
+        
+        # Convolution Block 2
+        x = self.convblock2(x)
+        
+        # Transition Block 2
+        x = self.transblock2(x)
+        
+        # Transition Block 3
+        x = self.convblock3(x)
+        
+        # Output Block
+        x = self.out(x) 
 
         x = x.view(-1, 10)
-        return F.log_softmax(x)
+        return F.log_softmax(x, dim=-1)
